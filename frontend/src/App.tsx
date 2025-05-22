51479621d8b226456455ecdf7056725bab5c4e1b
Guilherme020205 <seemannguilherme@gmail.com>
2025-05-22 18:31:26 -0300

    font Monstserrat
    Baixando a font Monstserrat, implementando pelo tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,13 @@ function App() {
 
   return (
     <>
-     <p className="bg-red-800">Olá mundo!</p>
+     <p className="font-montserrat font-bold text-4xl">36 PX BOLD</p>
+     <p className="font-montserrat font-bold text-2xl">24 PX BOLD</p>
+     <p className="font-montserrat font-bold text-[18px]">18 PX BOLD</p>
+     <p className="font-montserrat font-bold text-[14px]">14 PX BOLD</p>
+     <p className="font-montserrat font-normal text-[14px]">14 PX REGULAR</p>
+     <p className="font-montserrat font-normal text-xs">12 PX REGULAR</p>
+     <p className="font-montserrat font-normal text-[10px]">10 PX REGULAR</p>
     </>
   )
 }
